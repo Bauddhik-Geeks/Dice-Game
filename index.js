f89b1994a354e41a6762c6f872edfc7b153dc8c9
fenil3357 <fenil3357@gmail.com>
2021-10-01 00:10:02 +0530

    Added Name Feature

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-var randomNumber1=Math.floor(Math.random()*6 +1); //1-6
+
+ const name1 = prompt("Enter the name of the player 1")
+ const name2 = prompt("Enter the name of the player 2")
+
+
+ document.getElementById("btn").onclick = function() {
+   
+  var randomNumber1=Math.floor(Math.random()*6 +1); //1-6
 var randomNumber2=Math.floor(Math.random()*6 +1)  //1-6
 var randomDiceImage1 = "dice" + randomNumber1 + ".png";  //dice1.png  - dice6.png
 var randomImgsource1 = "images/" + randomDiceImage1;  //imag//dice1 - img/dice-6.png
@@ -13,15 +20,20 @@ var randomImgsource2 = "images/" + randomDiceImage2;  //imag//dice1 - img/dice-6
  img2.setAttribute("src",randomImgsource2);
 
 
+
 if(randomNumber1>randomNumber2)
 {
-  document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+  document.querySelector("h1").innerHTML = `🚩 ${name1} Wins!`;
 }
 else if(randomNumber2>randomNumber1)
 {
-  document.querySelector("h1").innerHTML="Player 2 Wins! 🚩";
+  document.querySelector("h1").innerHTML=`${name2} Wins! 🚩`;
 }
 else
 {
   document.querySelector("h1").innerHTML="It's Draw!";
 }
+
+}
+
+
