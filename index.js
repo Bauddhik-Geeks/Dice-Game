38216c0263740ea766bd681723b8ad4f2f930427
Kamlesh Joshi <72374645+kamleshjoshi8102@users.noreply.github.com>
2021-10-04 08:16:06 +0530

    Revert "Updated theme similar to main website"

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-function rollagains(){
 var randomNumber1=Math.floor(Math.random()*6 +1); //1-6
 var randomNumber2=Math.floor(Math.random()*6 +1)  //1-6
 var randomDiceImage1 = "dice" + randomNumber1 + ".png";  //dice1.png  - dice6.png
@@ -6,10 +5,7 @@ var randomImgsource1 = "images/" + randomDiceImage1;  //imag//dice1 - img/dice-6
 
 var randomDiceImage2 = "dice" + randomNumber2 + ".png";  //dice1.png  - dice6.png
 var randomImgsource2 = "images/" + randomDiceImage2;  //imag//dice1 - img/dice-6.png
-// var audio =new Audio("src/audio.mp3");
 
-
-// audio.play();
  var img1=document.querySelectorAll("img")[0];
  var img2=document.querySelectorAll("img")[1];
 
@@ -19,16 +15,13 @@ var randomImgsource2 = "images/" + randomDiceImage2;  //imag//dice1 - img/dice-6
 
 if(randomNumber1>randomNumber2)
 {
-  document.querySelector("h1").innerHTML = " 👇 Player 1 Wins!";
+  document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
 }
 else if(randomNumber2>randomNumber1)
 {
-  document.querySelector("h1").innerHTML="Player 2 Wins! 👇";
+  document.querySelector("h1").innerHTML="Player 2 Wins! 🚩";
 }
 else
 {
   document.querySelector("h1").innerHTML="It's Draw!";
 }
-
-}
-
